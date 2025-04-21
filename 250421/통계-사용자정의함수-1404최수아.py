8a5c4a711e8227674ef758a99e773ee36cd1d61b
ys0v9 <sua9097@gmail.com>
2025-04-21 10:39:23 +0900

    통계-사용자정의함수

--- 250421/통계-사용자정의함수-1404최수아.py
+++ 250421/통계-사용자정의함수-1404최수아.py
@@ -1,31 +1,37 @@
 import math
 
-#평균
+# 입력
+def f_input():
+    lst = list(map(int, input().split()))
+    return lst
+
+# 평균
 def f_avg(lst):
     mean = sum(lst) / len(lst)
-    return (mean)
+    return mean
 
-#분산
+# 분산
 def f_var(lst):
-    vsum=0
-    for x in lst:
-        vsum += (x-f_avg(lst)) ** 2
+    mean = f_avg(lst)
+    vsum = sum((x - mean) ** 2 for x in lst)
     var = vsum / len(lst)
-    return (var)
+    return var
 
-#표준편차
+# 표준편차
 def f_std(lst):
     std = math.sqrt(f_var(lst))
-    return (std)
+    return std
 
-#최대공약수, 최소공배수
+# 최대공약수, 최소공배수
 def f_comm(lst):
     gcd = math.gcd(*lst)
     lcm = math.lcm(*lst)
     return gcd, lcm
 
-lst = list(map(int,input().split()))
-print(f'평균: {f_avg(lst)}')
-print(f'분산: {f_var(lst)}')
-print(f'표준편차: {f_std(lst)}')
-print(f'최대공약수, 최소공배수: {f_comm(lst)}')
+data = f_input()
+
+print(f'평균: {f_avg(data)}')
+print(f'분산: {f_var(data)}')
+print(f'표준편차: {f_std(data)}')
+gcd, lcm = f_comm(data)
+print(f'최대공약수: {gcd}, 최소공배수: {lcm}')
