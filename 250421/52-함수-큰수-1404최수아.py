df5cc0acec62155659ac6d6e00041dbf72d5683a
ys0v9 <sua9097@gmail.com>
2025-04-21 10:12:09 +0900

    52-함수-큰수

--- 250421/52-함수-큰수-1404최수아.py
+++ 250421/52-함수-큰수-1404최수아.py
@@ -1,5 +1,10 @@
-def com():
-    print(f'큰 수: {max(a,b)}')
+def maxp(x,y):
+    if x >= y:
+        return(x)
+    else:
+        return(y)
+
+print(f'큰 수: {maxp(10,20)}')
 
 a, b = map(int,input().split())
-com()
+print(f'큰 수: {maxp(a, b)}')
