0d16428dadd57fe6231ecdf8a2653d929b46ff93
ys0v9 <sua9097@gmail.com>
2025-04-22 11:05:35 +0900

    54-재귀함수

---
+++ 250421/54-재귀함수-1404최수아.py
@@ -0,0 +1,26 @@
+# #함수 정의
+# def fun_s(n):
+#     if n<2:
+#         return 1
+#     else:
+#         return n + fun_s(n-1)
+    
+# #메인
+# print(fun_s(1))
+# print(fun_s(10))
+# a = int(input('자연수: '))
+# print(fun_s(a))
+
+
+#함수 정의
+def fun_f(m):
+    if m<2:
+        return 1
+    else:
+        return m * fun_f(m-1)
+
+#메인
+print(fun_f(1))
+print(fun_f(10))
+b = int(input('자연수: '))
+print(fun_f(b))
