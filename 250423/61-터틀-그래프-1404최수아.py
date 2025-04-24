22ada6948855310973219d98a31e03792c4442ec
ys0v9 <sua9097@gmail.com>
2025-04-24 11:11:09 +0900

    61-터틀-그래프

---
+++ 250423/61-터틀-그래프-1404최수아.py
@@ -0,0 +1,45 @@
+from turtle import *
+import random as r
+
+x_min = -5
+x_max = 5
+y_min = -5
+y_max = 5
+space = 0.1
+func_list = ["x*x", "abs(x)", "0.5*x+1"]
+
+setworldcoordinates(x_min, y_min, x_max, y_max)
+speed(0)
+pensize(3)
+
+up()
+goto(x_min, 0)
+down()
+goto(x_max, 0)
+
+up()
+goto(0, y_min)
+down()
+goto(0, y_max)
+
+for func in func_list:
+    color(r.choice(["green", "red", "blue", "purple"]))
+    x = x_min
+    up()
+    try:
+        y = eval(func)
+        goto(x, y)
+        down()
+    except:
+        continue
+
+    while x <= x_max:
+        try:
+            y = eval(func)
+            goto(x, y)
+        except:
+            pass
+        x += space
+
+done()
+
