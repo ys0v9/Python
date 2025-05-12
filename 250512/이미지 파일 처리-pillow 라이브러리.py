df7b37eb1831367a55ea09b248e9e499a28b1e0b
ys0v9 <sua9097@gmail.com>
2025-05-12 15:54:39 +0900

    이미지 파일 처리-pillow 라이브러리

--- 250512/이미지 파일 처리-pillow 라이브러리.py
+++ 250512/이미지 파일 처리-pillow 라이브러리.py
@@ -9,3 +9,14 @@ img.show() # 기본 이미지 뷰어로 열기
 print("이미지 크기:", img.size) # 크기는 튜플형(가로, 세로)
 print("이미지 형식:", img.format) # JPEG, PNG 등
 print("이미지 모드:", img.mode) # RGB, L(흑백) 등
+
+# 이미지 크기 조절
+resized = img.resize((220, 234))
+resized.show()
+
+# 이미지 흑백 변환
+gray = img.convert("L")
+gray.show()
+
+# 이미지 저장
+gray.save("gray_39a1df6c-0d2b-419e-aeb3-5037e038784a.jpeg")
